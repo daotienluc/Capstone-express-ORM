79114529d51b9ff26755546fec9f10b0ef45ab09
minh_tien <tien.dao@kynaforkids.vn>
2025-01-22 14:30:20 +0700

    Add func get user info by image id

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -11,6 +11,16 @@ const userController = {
       next(error);
     }
   },
+  getUserInfoByImageId: async (req, res, next) => {
+    const { image_id } = req.query
+    try {
+      const data = await userService.getUserInfoByImageId(image_id);
+      const resData = responseSuccess(data, `Get user info successfully`, 200);
+      res.status(resData.code).json(resData)
+    } catch (error) {
+      next(error)
+    }
+  }
 };
 
 export default userController;
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -2,4 +2,5 @@ import express from "express";
 import userController from "../controllers/userControllers.js";
 const userRouter = express.Router();
 userRouter.get("/getAllUser", userController.getAllUser);
+userRouter.get("/getUserInfo", userController.getUserInfoByImageId);
 export default userRouter;
